c1c472b6fe1bca52a06e41359d5b4991188e77ba
tvanvier-GMU <tvanvier@gmu.edu>
2016-10-30 00:00:03 -0400

    Initial Commit

---
+++ SimonController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SimonController : MonoBehaviour {
+
+	public bool enableInput = false;
+
+	public float stepPlaybackTime = .5f;
+
+
+	public Button[] InputButtons;
+	public AudioSource[] buttonAudioSources;
+	public UnityEngine.UI.Image[] buttonBaseSprites;
+	public UnityEngine.UI.Image[] buttonHighlightSprites;
+
+	public List<Color> colorPallets;
+
+	public AudioSource WrongBuzzer;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void CompareInput(int playerInput){
+
+	}
+
+}
